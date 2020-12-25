92d49ff959d83811cb474b85f0d5f6c69fb4a41e
genejo <eugen.goriac@me.com>
2020-12-24 16:08:11 -0800

    First commit

---
+++ HelloReactiveBlazor/ViewModels/ReactiveHelloViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Threading;
+using ReactiveUI;
+
+namespace HelloReactiveBlazor.ViewModels {
+    public class ReactiveHelloViewModel : ReactiveObject, IActivatableViewModel {
+        public ViewModelActivator Activator { get; }
+
+        private string _greeting;
+        public string Greeting {
+            get => _greeting;
+            set => this.RaiseAndSetIfChanged(ref _greeting, value);
+        }
+
+        public ReactiveHelloViewModel() {
+            Activator = new ViewModelActivator();
+ 
+            this.WhenActivated(
+                disposables => {
+                    Observable
+                        .Timer(
+                            TimeSpan.Zero,
+                            TimeSpan.FromMilliseconds(500),
+                            /*
+                            RxApp.MainThreadScheduler
+                            /*/
+                            RxApp.TaskpoolScheduler
+                            //*/
+                            )
+                        .Take(5)
+                        .Do(
+                            t => { Greeting = $"Hello, {t}!"; },
+                            () => Console.WriteLine("Those are all the greetings, folks! ")
+                        )
+                        .Subscribe()
+                        .DisposeWith(disposables);
+                });
+        }
+    }
+}
---
+++ HelloReactiveBlazor/Views/ReactiveHelloView.razor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Threading;
+using HelloReactiveBlazor.ViewModels;
+using ReactiveUI;
+
+namespace HelloReactiveBlazor.Views {
+    public partial class ReactiveHelloView {
+        public ReactiveHelloView()
+        {
+            ViewModel = new ReactiveHelloViewModel();
+
+            this.WhenActivated(disposables =>
+            {
+                Console.WriteLine("view activated");
+                Disposable
+                    .Create(() => Console.WriteLine("view deactivated"))
+                    .DisposeWith(disposables);
+
+                this
+                    .WhenAnyValue(v => v.ViewModel.Greeting)
+                    .Do(greeting => { Console.WriteLine($"view: {greeting}"); })
+                    .Subscribe();
+            });
+        }
+    }
+}
